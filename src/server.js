10b747b3366f8dba56026f39a44898d8f2bccb6e
amirshayanyian <wahtamir12@gmail.com>
2024-06-26 23:12:32 +0330

    added chalk package

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const { MainRouter } = require('./routes/router');
-require('dotenv').config();
 const mmd = require('./configs/model.config');
 const notFound = require('./middlewares/notFound.handler');
 const errorHandler = require('./middlewares/globalError.handler');
+require('dotenv').config();
 mmd();
 const port = process.env.SERVER_PORT || 3000;
 const app = express();
@@ -12,7 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(MainRouter);
 app.use(notFound);
-app.use(errorHandler)
-
+app.use(errorHandler);
 
 module.exports = { app, port };
