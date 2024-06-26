76ce1b4cc1a3b6942fac6c6bbb8690c9317fd154
amirshayanyian <wahtamir12@gmail.com>
2024-06-26 23:08:58 +0330

    added global error handler middleware

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { MainRouter } = require('./routes/router');
 require('dotenv').config();
 const mmd = require('./configs/model.config');
+const notFound = require('./middlewares/notFound.handler');
+const errorHandler = require('./middlewares/globalError.handler');
 mmd();
 const port = process.env.SERVER_PORT || 3000;
 const app = express();
@@ -9,5 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(MainRouter);
+app.use(notFound);
+app.use(errorHandler)
+
 
 module.exports = { app, port };
