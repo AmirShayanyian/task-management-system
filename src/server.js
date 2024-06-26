0ce79c7678ffab2309c13cd71e951808b7524a0b
amirshayanyian <wahtamir12@gmail.com>
2024-06-26 11:24:37 +0330

    added a single route

--- src/server.js
+++ src/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 require('dotenv').config();
+
 const port = process.env.SERVER_PORT || 3000;
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (req, res, next) => {
+  try {
+    return res.json({
+      message: `Welcome to Amir's task manager`,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = { app, port };
